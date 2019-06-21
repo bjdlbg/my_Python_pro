261448d221e2dbc28838291e1456e640618e4116
jixiang <1610252713@vip.henu.edu.cn>
2019-06-21 17:58:06 +0800

    Initial commit

---
+++ day01/学习数据类型.py
@@ -0,0 +1,34 @@
+import random
+
+# 使用random生成一个0-99的随机整数
+i = random.randrange(0, 99)
+print("hello"
+      ",你"
+      "好")
+print("这是一个随机数",i)
+# def setText:
+
+'''
+这是一段长注释
+'''
+"""
+这也是一段长注释
+"""
+# 声明一个整型
+a = 1
+
+# 声明一个浮点数
+b = 1000.1
+
+# 声明一个布尔型
+c = True
+
+# 声明一个字符串
+d = "这是一个字符串"
+
+print("a的类型", type(a))
+if c:
+    print("你好，智障")
+else:
+    print("不好,我不是智障")
+    print("nihao")
